f03fcedb9f3cbabbc3c4c137c2666f6f474ec617
NgocHaiThach <ngchai2410@gmail.com>
2022-09-15 16:31:25 +0700

    add boostrap and migrate table users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-    def index
-      render html: "users all"
+    def new
+    end
+
+    def show
+      @user = User.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #  resources :users
-   root 'application#home'
+  root 'static_pages#home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+  # get 'user'
+
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  resources :users
 end
