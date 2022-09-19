2f347cc75ffc7906e1f079befae2d1db1863eb74
NgocHaiThach <ngchai2410@gmail.com>
2022-09-19 11:23:19 +0700

    done remember user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_073414) do
+ActiveRecord::Schema.define(version: 2022_09_19_020557) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_09_15_073414) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
