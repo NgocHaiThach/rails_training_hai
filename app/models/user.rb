615ee7bcd18ae90da52b87741d1383ca18031f11
NgocHaiThach <ngchai2410@gmail.com>
2022-09-24 13:33:25 +0700

    done following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,12 @@ class User < ActiveRecord::Base
     
     #assiciation
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent:   :destroy
+    has_many :following, through: :active_relationships, source: :followed  
+
+    has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
+    has_many :followers, through: :passive_relationships, source: :follower
+
 
     #class method
     def User.digest (string)
@@ -75,7 +81,23 @@ class User < ActiveRecord::Base
     end
 
     def feed
-        Micropost.where("user_id = ?", id) 
+        following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids})
+        OR user_id = :user_id", user_id: id)
+    end
+
+
+    # Follows a user.
+    def follow(other_user) 
+        following << other_user
+    end
+    # Unfollows a user.
+    def unfollow(other_user) 
+        following.delete(other_user)
+    end
+    # Returns true if the current user is following the other user.
+    def following?(other_user) 
+        following.include?(other_user)
     end
 
     private
